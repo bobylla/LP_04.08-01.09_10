54ba055b9cd7ddedd5e9e5cff211938db9336c05
Владимир <Geth0s@mail.ru>
2022-08-06 13:49:22 +0900

    Обновлено Task0

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine()); // -2 147 483 648 до 2 147 483 647
+int square = number * number;
+if ( number == 5) // если равно
+{
+
+}
+if ( number != 5) // если не равно
+{
+    
+}
+Console.Write($"Квадрат числа {number} = {square}");
+
